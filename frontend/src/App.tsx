d7295cf80f821702c5cc919ef276af11016fe91b
Prantor-Das <prantor.college@gmail.com>
2025-10-26 14:48:11 +0530

    Add Share and Plus icons, update Button component to support icon props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from "./componenets/Button"
+import { PlusIcon } from "./icons/PlusIcon"
+import { ShareIcon } from "./icons/ShareIcon"
 
 function App() {
 
   return (
-    <div>
-      <Button variant="primary" size="sm" text={"Share Brain"} />
+    <div className="flex gap-1">
+      <Button startIcon={<ShareIcon size={"md"} />} variant="primary" size="sm" text={"Share Brain"} />
+      <Button startIcon={<PlusIcon size={"md"} />} variant="secondary" size="sm" text={"Add Content"} />
     </div>
   )
 }
--- frontend/src/componenets/Button.tsx
+++ frontend/src/componenets/Button.tsx
@@ -2,32 +2,40 @@
 // should not use it startIcon: any;
 // startIcon?: React.ReactElement;
 export interface ButtonProps {
-    variant: "primary" | "secondary";
-    size: "sm" | "md" | "lg";
-    text: string;
-    startIcon?: React.ReactNode;
-    endIcon?: React.ReactNode;
-    onClick?: () => void;
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  text: string;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  onClick?: () => void;
 }
 
 const variantStyle = {
-  "primary": "bg-violet-600 text-white hover:bg-violet-700",
-  "secondary": "bg-violet-200 text-blue-600 hover:bg-violet-300"
-}
+  primary: "bg-violet-600 text-white hover:bg-violet-700",
+  secondary: "bg-violet-200 text-violet-500 hover:bg-violet-300",
+};
 
-const variantSize ={
-  "sm": "px-3 py-2",
-  "md": "px-4 py-2",
-  "lg": "px-5 py-2"
-}
+const variantSize = {
+  sm: "px-3 py-2",
+  md: "px-4 py-2",
+  lg: "px-5 py-2",
+};
 
-const defaultStyle = "flex gap-2 rounded-lg text-lg items-center justify-center text-xl"
+const defaultStyle =
+  "flex gap-2 rounded-lg text-lg items-center justify-center text-xl";
 
 export const Button = (props: ButtonProps) => {
-  return <button  onClick={props.onClick}
-  className={`${variantStyle[props.variant]} ${variantSize[props.size] } ${defaultStyle}`}>
-    {props.startIcon ? props.startIcon : null} 
-    {props.text}
-    {props.endIcon ? props.endIcon: null}
-  </button>
+  return (
+    <button
+      onClick={props.onClick}
+      className={`
+  ${variantStyle[props.variant]} 
+  ${variantSize[props.size]} 
+  ${defaultStyle}`}
+    >
+      {props.startIcon ? props.startIcon : null}
+      {props.text}
+      {props.endIcon ? props.endIcon : null}
+    </button>
+  );
 };
